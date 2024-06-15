ece09be68311158472fcd003dbe3d3799cb61319
Samuel Santana <contato.santanasamuel@gmail.com>
2024-06-15 10:29:21 -0300

    feat: adicionando models e seus relacionamentos

---
+++ src/main/java/com/bookstore/jpa/models/AuthorModel.java
@@ -0,0 +1,37 @@
+package com.bookstore.jpa.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_AUTHOR")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToMany(mappedBy = "authors", fetch = FetchType.LAZY)
+    private Set<BookModel> books = new HashSet<>();
+
+}
---
+++ src/main/java/com/bookstore/jpa/models/ReviewModel.java
@@ -0,0 +1,37 @@
+package com.bookstore.jpa.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_REVIEW")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToOne
+    @JoinColumn(name = "book_id")
+    private BookModel book;
+
+
+}
